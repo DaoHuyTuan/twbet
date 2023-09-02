3a5c49dde99dc82db5288ef1519c0d885ede2fd4
daohuytuan <tuandh95@gmail.com>
2023-09-03 04:26:25 +0700

    done deposit

--- src/Pool.sol
+++ src/Pool.sol
@@ -4,37 +4,53 @@ pragma solidity ^0.8.13;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.1/contracts/token/ERC20/utils/SafeERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.1/contracts/token/ERC20/ERC20.sol";
 
+
 contract DeBetPool {
-    using SafeERC20 for ERC20;
+    
     struct Vault {
         address user;
         uint256 value;
+        uint256 amount;
     }
 
     struct Pool {
-        uint256 pool_id;
-        uint256 participants;
+        bytes32 pool_id;
         Vault[] vaults;
-        string status;
+        // string status;
     }
-    
-    function deposit() external payable {
-        // The amount of Ether sent is available as msg.value
-        uint256 amount = msg.value;
+    event LogMessage(bytes32 message);
+    using SafeERC20 for ERC20;
+    Pool public pool;
+    Vault[] public vaults;
+    uint256 participants = 0;
 
-        // You can now handle the deposited amount as needed
-        // For example, you could keep track of the sender's balance in the contract
-        // or perform other actions.
 
-        // Emit an event to log the deposit
-        emit Deposit(msg.sender, amount);
+    function viewTime() public view returns (uint256 time) {
+        return participants;
+    }
+
+    function deposit(uint256 lottery_id) external payable  {
+        Vault memory vault = Vault(msg.sender,lottery_id, msg.value);
+        vaults.push(vault);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function getVaultsInfo() public view returns (Vault[] memory) {
+        uint256 vaultsCount = vaults.length;
+        Vault[] memory result = new Vault[](vaultsCount);
+
+        for (uint256 i = 0; i < vaultsCount; i++) {
+            result[i] = vaults[i];
+        }
+        return result;
     }
 
     // Event to log deposits
     event Deposit(address indexed sender, uint256 amount);
 
-    function withdraw(ERC20 token, uint256 amount) public {
-        token.transfer(msg.sender, amount);
+    function withdraw() payable public {
+        payable(msg.sender).transfer(msg.value);
+        // token.transfer(msg.sender, amount);
     }
 
     function claim(uint256 value, address acc, uint256 pool_id) public {}
